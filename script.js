f7edbbd05c3e984d9fd558b4cea5e8aae2be7941
Avantika10589 <127646223+Avantika10589@users.noreply.github.com>
2025-09-11 10:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let table=document.getElementById("sampleTable");
+
+
 function insert_Row() {
     //Write your code here
-  
-  
+let row=table.insertRow(0);
+
+let td1=row.insertCell(0);
+let td2=row.insertCell(1);
+
+td1.textContent="New Cell1";
+td2.textContent="New Cell2";
+
+ 
+ 
 }
